8796090fb4a88bb11be835629cad3d38bb7df890
Alexandre Teixeira <alexanddy99@gmail.com>
2018-10-03 16:05:32 -0300

    melhorias

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
-    app.get('/produtos', function (req, res) {
 
+    var listaProdutos = function (req, res) {
         const connection = app.infra.connectionFactory();
         const produtoDAO = new app.infra.ProdutosDAO(connection);
 
@@ -11,23 +11,24 @@ module.exports = function (app) {
                 res.render('produtos/lista', { lista: results.rows });
             }
             connection.end();
-        })
-    });
+        });
+    }
 
-    app.get('/form', function (req, res) {
+    app.get('/produtos', listaProdutos);
+
+    app.get('/produtos/form', function (req, res) {
         res.render('produtos/form');
     });
 
-    app.post('/produtos/salva', function (req, res) {
+    app.post('/produtos', function (req, res) {
         const produto = req.body;
-
-        console.log(produto);
 
         const connection = app.infra.connectionFactory();
         const produtoDAO = new app.infra.ProdutosDAO(connection);
 
         produtoDAO.salva(produto, function (erros, resultados) {
-            res.render('produtos/lista');
+            connection.end();
+            res.redirect('/produtos');
         });
     });
 };
