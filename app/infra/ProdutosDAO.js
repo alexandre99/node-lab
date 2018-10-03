10687b744c1cd23661c052c7ba11c25d348c02b5
Alexandre Teixeira <alexanddy99@gmail.com>
2018-10-03 10:54:20 -0300

    salvando dados pelo formulario

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -1,12 +1,21 @@
-function ProdutosDAO(connection){
+function ProdutosDAO(connection) {
     this._connection = connection;
 }
 
-ProdutosDAO.prototype.lista = function(callback) {
+ProdutosDAO.prototype.lista = function (callback) {
     this._connection.connect();
     this._connection.query('select * from livros', callback);
 }
 
-module.exports = function(){
+ProdutosDAO.prototype.salva = function (produto, callback) {
+    this._connection.connect();
+    const query = {
+        text: 'INSERT INTO livros(titulo, descricao, preco) VALUES ($1, $2, $3)',
+        values: [produto.titulo, produto.descricao, produto.preco],
+    }
+    this._connection.query(query, callback);
+}
+
+module.exports = function () {
     return ProdutosDAO;
 }
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,16 +1,33 @@
-module.exports = function(app) {
-  app.get('/produtos', function(req, res) {
-    
-    const connection = app.infra.connectionFactory();
-    const produtoDAO = new app.infra.ProdutosDAO(connection);
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
 
-    produtoDAO.lista(function(err, results) {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.render('produtos/lista', {lista: results.rows});
-        }
-        connection.end();
-    })
-  });
+        const connection = app.infra.connectionFactory();
+        const produtoDAO = new app.infra.ProdutosDAO(connection);
+
+        produtoDAO.lista(function (err, results) {
+            if (err) {
+                console.log(err.message);
+            } else {
+                res.render('produtos/lista', { lista: results.rows });
+            }
+            connection.end();
+        })
+    });
+
+    app.get('/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', function (req, res) {
+        const produto = req.body;
+
+        console.log(produto);
+
+        const connection = app.infra.connectionFactory();
+        const produtoDAO = new app.infra.ProdutosDAO(connection);
+
+        produtoDAO.salva(produto, function (erros, resultados) {
+            res.render('produtos/lista');
+        });
+    });
 };
